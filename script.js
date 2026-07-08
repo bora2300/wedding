9877b28a33b896f72c3f2e347475bcd19eb3f9cc
bora2300 <b_0ra@kakao.com>
2026-07-08 22:49:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -181,21 +181,20 @@ function closeModal() {
 }
 
 function changePhoto(direction) {
-  currentIndex = currentIndex + direction;
+    currentIndex = currentIndex + direction;
 
-  if (currentIndex < 0) {
-    currentIndex = galleryImages.length - 1;
-  }
+    if (currentIndex < 0) {
+        currentIndex = galleryImages.length - 1;
+    }
 
-  if (currentIndex >= galleryImages.length) {
-    currentIndex = 0;
-  }
+    if (currentIndex >= galleryImages.length) {
+        currentIndex = 0;
+    }
 
-  document.getElementById("modal-img").src =
-    galleryImages[currentIndex];
+    document.getElementById("modal-img").src = galleryImages[currentIndex];
 
-document.getElementById("modal-counter").innerText =
-    (currentIndex + 1) + " / " + galleryImages.length;
+    document.getElementById("modal-counter").innerText =
+        (currentIndex + 1) + " / " + galleryImages.length;
 }
 
 let startX = 0;
